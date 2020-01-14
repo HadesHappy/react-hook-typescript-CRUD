04532706a71dff4c8b769fc965bd21761818d453
HadesHappy <happyhades123@gmail.com>
2020-01-14 10:18:47 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter as Router, Route, Switch, Link  } from 'react-router-dom'
+import { StudentPage } from './Student/StudentPage';
+import { StudentPageExtended } from './StudentExtended/StudentPage';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<Router>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Documentation</Link>
+					</li>
+					<li>
+						<Link to="/student">Student</Link>
+					</li>
+					<li>
+						<Link to="/student-extended" className="push-right">Student Extended</Link>
+					</li>
+				</ul>				
+			</nav>
+			<div>
+				<Switch>
+					<Route exact path="/">
+						<ul>
+							<li>IStudent extends IEntity</li>
+							<li>IStudentState extends IEntityState</li>
+						</ul>
+					</Route>
+					<Route path="/student">
+						<StudentPage query="all" />
+					</Route>
+					<Route path="/student-extended">
+						<StudentPageExtended query="all" />
+					</Route>						
+				</Switch>
+			</div>	
+		</Router>
   );
 }
 
